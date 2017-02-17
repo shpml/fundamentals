d491ddcfbd185fe8b59b3f62dee18c6b4a1f1af6
sarah hallacher <sarah.hallacher@generalassemb.ly>
2017-02-17 11:06:08 -0500

    removing v3.1

--- v3.1/unit_assignments/07-assignment/js/main.js
+++
@@ -1,9 +0,0 @@
-// Create variables to store each card
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-
-// Use console.log() to display the two cards that the user flipped
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
